a289f182ba261858d252d0d6da221d50b84f2dac
santbetv <santiago-betancur@hotmail.com>
2023-10-16 21:54:57 -0500

    feat agregando proyecto principal cuenta y movimiento

---
+++ src/main/java/com/devsu/cuentamovimiento/CuentamovimientoApplication.java
@@ -0,0 +1,13 @@
+package com.devsu.cuentamovimiento;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CuentamovimientoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CuentamovimientoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/devsu/cuentamovimiento/CuentamovimientoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devsu.cuentamovimiento;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CuentamovimientoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
